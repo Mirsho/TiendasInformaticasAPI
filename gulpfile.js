d1f89074c2c87c33f48c499261f738c583354294
Mirsho <mirsho@protonmail.com>
2021-02-11 13:21:02 +0000

    Segunda corrección del archivo gulpfile - Gulp-eslint totalmente operativo

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,15 @@ gulp.task('doc', function (cb) {
 });
 
 gulp.task('lint', () => {
-  gulp.src(['./js/*.js'])
+  return gulp.src(['./js/*.js'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
     .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
     .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
 });
 
