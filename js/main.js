8fec19734c784e0456df98a61efac5e196ed1b82
Mirsho <mirsho@protonmail.com>
2021-02-11 09:44:21 +0000

    (Corregido) Gulp-eslint pulido, añadidas las peticiones

--- js/main.js
+++ js/main.js
@@ -158,29 +158,3 @@ const doTask = (iterations) => new Promise((resolve, reject) => {
     value: numbers
   });
 });
-
-doTask(10)
-  .then(result => console.log("Tiradas correctas: ", result.value))
-  .catch(err => console.error("Se ha sacado un ", err.message));
-
-  const doTask = (iterations) => new Promise((resolve, reject) => {
-    const numbers = [];
-    for (let i = 0; i < iterations; i++) {
-      const number = 1 + Math.floor(Math.random() * 6);
-      numbers.push(number);
-      if (number === 6) {
-        reject({
-          error: true,
-          message: "Se ha sacado un 6"
-        });
-      }
-    }
-    resolve({
-      error: false,
-      value: numbers
-    });
-  });
-  
-  doTask(10)
-    .then(result => console.log("Tiradas correctas: ", result.value))
-    .catch(err => console.error("Se ha sacado un ", err.message));
